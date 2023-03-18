10aeed31b43f80f34340ce92b86e110f156dd378
harimrim <kigkfla@naver.com>
2023-03-18 18:08:19 +0900

    PokerCard class 구현

--- PokerGameApp/PokerGameApp/GameBoardViewController.swift
+++ PokerGameApp/PokerGameApp/GameBoardViewController.swift
@@ -23,40 +23,79 @@ class GameBoardViewController: UIViewController {
         let cardLeftTopX:CGFloat = 0
         let cardRightTopY:CGFloat = 50
         let cardCount:CGFloat = 7
-        var positionX:CGFloat = cardLeftTopX
-        let cardWidth = (self.view.bounds.width / cardCount) - widthPadding
+        let viewWidth = self.view.bounds.width
+        let cardWidth = (viewWidth / cardCount) - widthPadding
         var cardBackImage:UIImage? = nil
         if let cardBack = UIImage(named: "card-back") {
             cardBackImage = cardBack
         }
         
-        for _ in 0 ... 6 {
-            var positionY:CGFloat = cardRightTopY
-            let cardBack: UIImageView = {
-                let cardHeight = cardWidth * ratio
-                let cardBack = UIImageView(frame: CGRect(x: positionX, y: positionY, width: cardWidth, height: cardHeight))
-                cardBack.image = cardBackImage
-                return cardBack
-            }()
-            self.view.addSubview(cardBack)
-            positionX += cardWidth + widthPadding
-        }
+        var cardStackView:UIStackView = UIStackView()
+            cardStackView.axis = .horizontal
+            cardStackView.alignment = .fill
+            cardStackView.distribution = .fillEqually
+            cardStackView.spacing = widthPadding
         
+        for _ in 0 ... 6 {
+            let cardBack:UIImageView = UIImageView()
+            cardBack.contentMode = .scaleAspectFit
+            cardBack.image = cardBackImage
+            cardStackView.addArrangedSubview(cardBack)
+        }
+        cardStackView.frame = CGRect(x: cardLeftTopX, y: cardRightTopY, width: viewWidth, height: cardWidth)
+        self.view.addSubview(cardStackView)
+
+           
+        
+
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-
+    var heartQ = PokerCard(suit: .spade, rank: .K)
+    
 }
 
 class PokerCard {
+    // 연관된 계층 구조를 깔끔하게 표현하기 위해서 nested Type 사용
     enum Suit: Character {
         case spade = "♠"
         case heart = "♥"
         case diamond = "♦"
         case club = "♣"
+    }
+    // 카드의 랭크는 추후 승패를 위해 비교해야 하니 Int Type 사용
+    enum Rank: Int {
+        case A = 1, two, three, four, five, six, seven, eight, nine, ten, J, Q, K
+        
+        func convert() -> String {
+            switch self {
+            case.A:
+                return "A"
+            case.J:
+                return "J"
+            case.Q:
+                return "Q"
+            case.K:
+                return "K"
+            default:
+                return String(self.rawValue)
+            }
+        }
+    }
+    
+    var suit:Suit
+    var rank:Rank
+    
+    init(suit:Suit, rank:Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+    
+    func cardDescription() -> String {
+        return "\(suit.rawValue)\(rank.convert())"
     }
 }
 
