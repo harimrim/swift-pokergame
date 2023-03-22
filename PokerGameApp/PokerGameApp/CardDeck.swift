0a868b5458c8868373a6ff2218d5bcb3b27d0807
harimrim <kigkfla@naver.com>
2023-03-22 15:53:18 +0900

    CardDeck 파일 분리 후 로직 작성

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct CardDeck {
+    private var cardDeck:[PokerCard]
+    
+    init() {
+        cardDeck = CardDeck.createDeck()
+    }
+    mutating func reset() {
+        cardDeck = CardDeck.createDeck()
+    }
+    
+    static func createDeck() -> [PokerCard] {
+        var deck = [PokerCard]()
+        let suits = PokerCard.Suit.allCases
+        let ranks = PokerCard.Rank.allCases
+        for suit in suits {
+            for rank in ranks {
+                let card = PokerCard(suit: suit, rank: rank)
+                deck.append(card)
+                }
+        }
+        return deck
+    }
+    
+    mutating func removeOne() -> PokerCard? {
+        guard let card = cardDeck.popLast() else {
+            return nil
+        }
+        return card
+    }
+    
+    mutating func shuffle() {
+        for i in stride(from: cardDeck.count - 1, through: 1, by: -1) {
+            let j = Int.random(in: 0..<i)
+            if i != j {
+                cardDeck.swapAt(i, j)
+            }
+        }
+    }
+    
+    func count() -> Int {
+        return cardDeck.count
+    }
+
+}
